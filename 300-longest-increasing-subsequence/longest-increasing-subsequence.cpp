4f46a6137d9b7c799049555e8a4f90b8795460d6
Hrishabh Joshi <108185291+Hrishabhjoshi@users.noreply.github.com>
2023-08-30 11:55:53 +0530

    Time: 568 ms (34.05%) | Memory: 38 MB (35.36%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int n;
+int dp[2501][2501];
+    int solve(vector<int>&nums, int i, int p)
+    {
+        if(i>=n)return 0;
+        int take=0;
+        if(p!=-1 && dp[i][p]!=-1)return dp[i][p];
+        if(p==-1 || nums[i]>nums[p]){
+            take= 1+solve(nums,i+1,i);
+        }
+        int leave= solve(nums, i+1, p);
+        if(p!=-1)dp[i][p]=max(take,leave);
+        return max(take,leave);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        
+        n= nums.size();
+        memset(dp,-1,sizeof(dp));
+        if(n==1)return 1;
+        int res= solve(nums,0, -1);
+        return res;
+    }
+};
