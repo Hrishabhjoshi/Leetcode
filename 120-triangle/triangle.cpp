ae35ef435d55a7d71a4747f499279a23a5849c08
Hrishabh Joshi <108185291+Hrishabhjoshi@users.noreply.github.com>
2024-01-10 11:09:03 +0530

    Time: 4 ms (77.74%) | Memory: 9.5 MB (18.78%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -15,4 +15,28 @@ private:
         int downRight = dp(triangle, idx + 1, row + 1, memo);
         return memo[row][idx] = triangle[row][idx] + min(down, downRight);
     }
+    //  public:
+    //  int solve(vector<vector<int>>&triangle, int row, int col,vector<vector<int>>dp)
+    //  {
+    //      int n= triangle.size();
+         
+    //      if(row==triangle.size())return 0;
+    //      if(dp[row][col]!=0)return dp[row][col];
+    //      int take, leave;
+    //      take= triangle[row][col]+solve(triangle, row+1, col,dp);
+    //      leave= triangle[row][col]+solve(triangle, row+1, col+1,dp);
+    //      return dp[row][col]=min(take,leave);
+
+    //  }
+    //  int minimumTotal(vector<vector<int>>& triangle)
+    //  {
+    //      int n= triangle.size();
+    //      vector<vector<int>>dp(n, vector<int>(n,0));
+    //      if(n==0)return 0;
+    //      if(n==1){
+    //          int mine= *min_element(triangle[0].begin(), triangle[0].end());
+    //          return mine;
+    //      }
+    //     return solve(triangle, 0,0,dp);
+    //  }
 };
